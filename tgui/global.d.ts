8c73a898c86e1fde22974d5671ab09e4db025098
CFWMagic <147959339+CFWMagic@users.noreply.github.com>
2026-05-13 14:59:18 +0200

    Fixes chat log save fuction
    - Implements the VORE & TG Station fixes
    - Research done by Ryuu
    - Implemented by CFW

--- tgui/global.d.ts
+++ tgui/global.d.ts
@@ -163,6 +163,12 @@ type ByondType = {
    * Maps icons to their ref
    */
   iconRefMap: Record<string, string>;
+
+  /**
+   * Downloads a blob, platform-agnostic
+   */
+  saveBlob(blob: Blob, filename: string, ext: string): void;
+
 };
 
 /**
